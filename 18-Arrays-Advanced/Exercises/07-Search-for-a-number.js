9e462429ccb50fe2cd18e4ab8e1f6fb81e5681cb
Kiril Yanev <kl_yanev@abv.bg>
2022-10-17 12:33:30 +0300

    07-Search-for-a-number

---
+++ 18-Arrays-Advanced/Exercises/07-Search-for-a-number.js
@@ -0,0 +1,26 @@
+function numSearch(array, operations) {
+    let takeElementsFromArray = operations[0];
+    let elementsToDelete = operations[1];
+    let searchedNum = operations[2];
+
+    //  First, we take 5 elements from the array. DONE
+    let newArray = array.slice(0, takeElementsFromArray);
+    console.log(newArray);
+
+    //  Delete the first 2 elements. DONE
+    newArray.splice(0, elementsToDelete,);
+    console.log(newArray);
+
+    //  Then we search for the number 3.
+    let count = 0;
+
+    newArray.forEach(element => {
+        if (element === searchedNum) {
+            count++
+        }
+    });
+
+    console.log(`Number ${searchedNum} occurs ${count} times.`);
+}
+numSearch([7, 1, 5, 8, 2, 7],
+    [3, 1, 5])
