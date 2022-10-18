9205afc83f925470578a7f9653b08d834c350f8d
Kiril Yanev <kl_yanev@abv.bg>
2022-10-18 20:17:54 +0300

    Mid-Exam-Problem 1

--- 18-Arrays-Advanced/Exercises/07-Search-for-a-number.js
+++ 18-Arrays-Advanced/Exercises/07-Search-for-a-number.js
@@ -5,13 +5,11 @@ function numSearch(array, operations) {
 
     //  First, we take 5 elements from the array. DONE
     let newArray = array.slice(0, takeElementsFromArray);
-    console.log(newArray);
 
     //  Delete the first 2 elements. DONE
     newArray.splice(0, elementsToDelete,);
-    console.log(newArray);
 
-    //  Then we search for the number 3.
+    //  Then we search for the number 3. DONE
     let count = 0;
 
     newArray.forEach(element => {
---
+++ 19-Mid-exam-preparation/01-Black-flag.js
@@ -0,0 +1,22 @@
+function plunderCalc(input) {
+    let days = Number(input[0]);
+    let plunderForDay = Number(input[1]);
+    let expectedPlunder = Number(input[2]);
+    let totalPlunder = 0;
+
+    for (let index = 1; index <= days; index++) {
+        totalPlunder += plunderForDay;
+        if (index % 3 === 0) {
+            totalPlunder += (plunderForDay * 0.50);
+        } else if (index % 5 === 0) {
+            totalPlunder -= (totalPlunder * 0.30);
+        }
+    }
+    if (totalPlunder >= expectedPlunder) {
+        console.log(`Ahoy! ${totalPlunder.toFixed(2)} plunder gained.`);
+    } else {
+        let percentPlunderCollect = (totalPlunder / expectedPlunder) * 100;
+        console.log(`Collected only ${percentPlunderCollect.toFixed(2)}% of the plunder.`);
+    }
+}
+plunderCalc(["10", "20", "380"]);
