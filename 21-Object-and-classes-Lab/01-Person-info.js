5c301cff472c2c95ee07bed60a11feb11ee08880
Kiril Yanev <kl_yanev@abv.bg>
2022-10-27 15:46:11 +0300

    Person-info from Object and Classes

---
+++ 21-Object-and-classes-Lab/01-Person-info.js
@@ -0,0 +1,11 @@
+function personInfo(firstName, lastName, age) {
+    let personalInformation = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+    }
+    return personalInformation;
+}
+personInfo("Peter", 
+"Pan",
+"20");
