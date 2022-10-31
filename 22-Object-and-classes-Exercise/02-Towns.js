85f2f4a1cc418b96f98748479f8c6276a1129ff9
Kiril Yanev <kl_yanev@abv.bg>
2022-10-31 23:47:51 +0200

    Update Towns solution

--- 22-Object-and-classes-Exercise/02-Towns.js
+++ 22-Object-and-classes-Exercise/02-Towns.js
@@ -2,9 +2,20 @@ function towns(input) {
     let townInfo = {};
 
     for (const line of input) {
-        console.log(line);
+        // let splitedLine = line.split(' | ')
+        // let townName = splitedLine[0];
+        // let latitude = splitedLine[1];
+        // let longitude = splitedLine[2];
+
+        let [townName, latitude, longitude] = line.split(' | ')
+
+        townInfo.town = townName;
+        townInfo.latitude = Number(latitude).toFixed(2);
+        townInfo.longitude = Number(longitude).toFixed(2);
+
+        console.log(townInfo);
     }
-    
 }
+
 towns(['Sofia | 42.696552 | 23.32601',
 'Beijing | 39.913818 | 116.363625']);
