d4c38f8f7c14202d95c72037768fe3322e8ead8b
Kiril Yanev <kl_yanev@abv.bg>
2022-10-31 23:24:44 +0200

    Create Towns

---
+++ 22-Object-and-classes-Exercise/02-Towns.js
@@ -0,0 +1,10 @@
+function towns(input) {
+    let townInfo = {};
+
+    for (const line of input) {
+        console.log(line);
+    }
+    
+}
+towns(['Sofia | 42.696552 | 23.32601',
+'Beijing | 39.913818 | 116.363625']);
