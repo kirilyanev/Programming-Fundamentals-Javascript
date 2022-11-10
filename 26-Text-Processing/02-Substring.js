9d48b372147cba3547262fa727e24d2e0e39fe0a
Kiril Yanev <kl_yanev@abv.bg>
2022-11-10 16:35:19 +0200

    Text Processing
    02 - Substring

---
+++ 26-Text-Processing/02-Substring.js
@@ -0,0 +1,5 @@
+function substringPrint(string, startIndex, count) {
+    let result = string.substring(startIndex, startIndex + count)
+    console.log(result);
+}
+substringPrint('ASentence', 1, 8);
