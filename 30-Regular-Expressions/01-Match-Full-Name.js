df04e2b61d605843f798e3020fbc740c3e068419
Kiril Yanev <kl_yanev@abv.bg>
2022-11-17 22:25:14 +0200

    30-Regular-Expressions
    01-Match-full-name

---
+++ 30-Regular-Expressions/01-Match-Full-Name.js
@@ -0,0 +1,11 @@
+function solve(names) {
+    const pattern = /\b[A-Z][a-z]+ [A-Z][a-z]+\b/g;
+    let validNames = [];
+
+    while ((validName = pattern.exec(names)) !== null) {
+        validNames.push(validName[0]);
+    }
+
+    console.log(validNames.join(' '));
+}
+solve("Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan	Ivanov")
