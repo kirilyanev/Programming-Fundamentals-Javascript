b9e769acef884f0e0fcc618024b5dd4d27881349
Kiril Yanev <kl_yanev@abv.bg>
2022-11-28 00:18:42 +0200

    31-Regular-Expression-Exercise
    04-Star-enigma after finding the mistake (the regex) and fixing it - 100/100 in judge

--- 31-Regular-Expressions-Exercise/04-Star-enigma.js
+++ 31-Regular-Expressions-Exercise/04-Star-enigma.js
@@ -31,7 +31,7 @@ function decrypt(messages) {
         // let attackType = attackRegex.exec(message);
         // let soldiers = soldierCountRegex.exec(message);
 
-        let regex = /@(?<planet>[A-za-z]+)[^@\-!:>]*:(?<population>\d+)!(?<attack>[AD])!->(?<soldiers>\d+)/gm;
+        let regex = /@(?<planet>[a-zA-Z]+)[^@\-!:>]*:(?<population>\d+)[^@\-!:>]*!(?<attack>[AD])![^@\-!:>]*->(?<soldiers>\d+)/gm
         let result = regex.exec(message)
 
         //console.log(`${result.groups['planet']} - ${result.groups['population']} - ${result.groups['attack']} - ${result.groups['soldiers']} `);
@@ -49,14 +49,18 @@ function decrypt(messages) {
     destroyedPlanets.sort((a, b) => a.localeCompare(b));
 
     console.log(`Attacked planets: ${attackedPlanets.length}`);
+
     attackedPlanets.forEach(planet => {
         console.log(`-> ${planet}`);
     });
+
     console.log(`Destroyed planets: ${destroyedPlanets.length}`);
+
     destroyedPlanets.forEach(planet => {
         console.log(`-> ${planet}`);
     });
 }
+
 decrypt(['2',
     'STCDoghudd4=63333$D$0A53333',
     'EHfsytsnhf?8555&I&2C9555SR']);
